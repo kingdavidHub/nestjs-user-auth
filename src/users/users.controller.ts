449415d2a6633a2255d281a94ff9852d6099ce05
David Igbayilola <Kingigbayilola@gmail.com>
2026-01-13 12:33:20 +0100

    feat: implement findUserById and deleteUserById methods in UsersService and update UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Delete, Get, ParseIntPipe, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { QueryUsersEmail } from './dto/users.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(readonly usersService: UsersService){}
+  constructor(readonly userService: UsersService){}
 
   @Get('')
   async findAllUsers(@Query() query: QueryUsersEmail){
-    return await this.usersService.findAllUsers(query.email);
+    return await this.userService.findAllUsers(query.email);
+  }
+
+  @Get(':id')
+  async findUserById(id: ParseIntPipe) {
+    return await this.userService.findUserById(+id);
+  }
+
+  @Delete(':id')
+  async deleteUserById(id:ParseIntPipe){
+    return await this.userService.deleteUserById(+id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,7 +7,6 @@ export class UsersService {
 
   async findAllUsers(userEmail: string) {
     try {
-
       // find a specific users
       if (userEmail) {
         const user = await this.prisma.user.findUnique({
@@ -37,6 +36,60 @@ export class UsersService {
         totalUsers,
         users,
       };
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException(
+        'Failed to fetch users',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async findUserById(id: number) {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: { id },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        },
+      });
+      return user;
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new HttpException(
+        'Failed to fetch users',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+
+  async deleteUserById(id: number){
+    try {
+      const user = await this.findUserById(id);
+      if(!user){
+        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      }
+
+      const deletedUser = await this.prisma.user.delete({
+        where: {id}
+      })
+    
+      if(!deletedUser){
+        throw new HttpException('Failed to delete user', HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+
+      return {
+        message: 'User deleted successfully',
+        user: deletedUser
+      }
+
     } catch (error) {
       if (error instanceof HttpException) {
         throw error;
